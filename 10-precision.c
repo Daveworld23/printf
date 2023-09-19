1004fe9892e28bb63bbfe4b39e692527461898e2
Torubiri Walter <toruwalt1997@gmail.com>
2023-09-19 12:34:36 -0700

    This handles the precision

---
+++ 10-precision.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * include_precision - Calculates the precision for printing
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: Precision.
+ */
+int include_precision(const char *format, int *i, va_list list)
+{
+	int j = *i + 1;
+	int precision = -1;
+
+	if (format[j] != '.')
+		return (precision);
+
+	precision = 0;
+
+	for (j += 1; format[j] != '\0'; j++)
+	{
+		if (digit(format[j]))
+		{
+			precision *= 10;
+			precision += format[j] - '0';
+		}
+		else if (format[j] == '*')
+		{
+			j++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = j - 1;
+
+	return (precision);
+}
