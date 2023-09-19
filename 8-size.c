5719760d07888305c8acf92df9d7ac5f56e1e76b
Torubiri Walter <toruwalt1997@gmail.com>
2023-09-19 12:07:54 -0700

    This gets the size

---
+++ 8-size.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * include_size -Calculates size
+ * @format: Formatted string in which to print the arguments
+ * @i: List of arguments to be printed.
+ * Return: Size.
+ */
+int include_size(const char *format, int *i)
+{
+	int j = *i + 1;
+	int size = 0;
+
+	if (format[j] == 'l')
+		size = SIZE_L;
+	else if (format[j] == 'h')
+		size = SIZE_S;
+
+	if (size == 0)
+		*i = j - 1;
+	else
+		*i = j;
+
+	return (size);
+}
